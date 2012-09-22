7ac7c3c8bfc5f77482541819ae97cafe2799840c
ArNo <ArNo@ArNo-PC>
2012-09-22 09:53:36 +0200

    Add JUnit test

---
+++ scrumble-server-ejb/src/test/java/com/scrumble/server/entities/Member1Test.java
@@ -0,0 +1,217 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.scrumble.server.entities;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ArNo
+ */
+public class Member1Test {
+    
+    public Member1Test() {
+    }
+
+    /*@org.junit.BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }*/
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    
+    /*@Test
+    public void testCountMember1() {
+        System.out.println("testCountMember1");
+        Member1 instance = new Member1();
+        Integer expResult = 5;
+        Integer result = instance.count();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }*/
+    
+    /**
+     * Test of getIdMember method, of class Member1.
+     */
+    /*@org.junit.Test
+    public void testGetIdMember() {
+        System.out.println("getIdMember");
+        Member1 instance = new Member1();
+        Integer expResult = null;
+        Integer result = instance.getIdMember();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }*/
+
+    /**
+     * Test of setIdMember method, of class Member1.
+     */
+    /*@org.junit.Test
+    public void testSetIdMember() {
+        System.out.println("setIdMember");
+        Integer idMember = null;
+        Member1 instance = new Member1();
+        instance.setIdMember(idMember);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }*/
+
+    /**
+     * Test of getFirstname method, of class Member1.
+     */
+    /*@org.junit.Test
+    public void testGetFirstname() {
+        System.out.println("getFirstname");
+        Member1 instance = new Member1();
+        String expResult = "";
+        String result = instance.getFirstname();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }*/
+
+    /**
+     * Test of setFirstname method, of class Member1.
+     */
+    /*@org.junit.Test
+    public void testSetFirstname() {
+        System.out.println("setFirstname");
+        String firstname = "";
+        Member1 instance = new Member1();
+        instance.setFirstname(firstname);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }*/
+
+    /**
+     * Test of getLastname method, of class Member1.
+     */
+    /*@org.junit.Test
+    public void testGetLastname() {
+        System.out.println("getLastname");
+        Member1 instance = new Member1();
+        String expResult = "";
+        String result = instance.getLastname();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }*/
+
+    /**
+     * Test of setLastname method, of class Member1.
+     */
+    /*@org.junit.Test
+    public void testSetLastname() {
+        System.out.println("setLastname");
+        String lastname = "";
+        Member1 instance = new Member1();
+        instance.setLastname(lastname);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }*/
+
+    /**
+     * Test of getLogin method, of class Member1.
+     */
+    /*@org.junit.Test
+    public void testGetLogin() {
+        System.out.println("getLogin");
+        Member1 instance = new Member1();
+        String expResult = "";
+        String result = instance.getLogin();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }*/
+
+    /**
+     * Test of setLogin method, of class Member1.
+     */
+    /*@org.junit.Test
+    public void testSetLogin() {
+        System.out.println("setLogin");
+        String login = "";
+        Member1 instance = new Member1();
+        instance.setLogin(login);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }*/
+
+    /**
+     * Test of hashCode method, of class Member1.
+     */
+    /*@org.junit.Test
+    public void testHashCode() {
+        System.out.println("hashCode");
+        Member1 instance = new Member1();
+        int expResult = 0;
+        int result = instance.hashCode();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }*/
+
+    /**
+     * Test of equals method, of class Member1.
+     */
+    /*@org.junit.Test
+    public void testEquals() {
+        System.out.println("equals");
+        Object object = null;
+        Member1 instance = new Member1();
+        boolean expResult = false;
+        boolean result = instance.equals(object);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }*/
+
+    /**
+     * Test of toString method, of class Member1.
+     */
+    /*@org.junit.Test
+    public void testToString() {
+        System.out.println("toString");
+        Member1 instance = new Member1();
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }*/
+}
