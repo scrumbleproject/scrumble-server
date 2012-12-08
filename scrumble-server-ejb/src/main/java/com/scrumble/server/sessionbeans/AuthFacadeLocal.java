0968fab5bc97d17f9725f21a3cccb89748b0844d
cypi <epicyr@gmail.com>
2012-12-08 11:09:33 +0100

    authentication and checkToken

--- scrumble-server-ejb/src/main/java/com/scrumble/server/sessionbeans/AuthFacadeLocal.java
+++ scrumble-server-ejb/src/main/java/com/scrumble/server/sessionbeans/AuthFacadeLocal.java
@@ -13,7 +13,21 @@ import javax.ejb.Local;
 @Local
 public interface AuthFacadeLocal {
 
-    
+    /**
+     * Allows user to authenticate with a login and a password
+     * @param login the login of the user
+     * @param password the password of the user 
+     * @return a token if authentication has been successful or null otherwise
+     */
     public String authenticate(String login, String password) throws Exception;
+    
+    
+    /**
+     * Check if token already exist or is not expired and authenticate otherwise
+     * @param login the login of authenticated user
+     * @param encryptKey the encrypted token passed in all user request
+     * @return true if token is active or false otherwise
+     */
+    public boolean checkToken(String login, String encryptKey) throws Exception;
     
 }
