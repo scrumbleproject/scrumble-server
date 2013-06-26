0d7638ad96cf7c5ad68c36aae7888cf89a1e804f
Cypi <epicyr@gmail.com>
2013-06-26 20:16:45 +0200

    update db with user passwords

--- scrumble-server-ejb/src/main/resources/DUMP/DB/scrumble_bdd.sql
+++ scrumble-server-ejb/src/main/resources/DUMP/DB/scrumble_bdd.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.7deb7
+-- version 3.4.11.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 22, 2013 at 05:57 PM
--- Server version: 5.1.66
--- PHP Version: 5.3.3-7+squeeze15
+-- Generation Time: Jun 26, 2013 at 08:11 PM
+-- Server version: 5.5.31
+-- PHP Version: 5.4.6-1ubuntu1.2
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -49,15 +50,15 @@ CREATE TABLE IF NOT EXISTS `member` (
 
 INSERT INTO `member` (`id_member`, `firstname`, `lastname`, `login`, `password`, `email`, `id_role`, `internal_phone`, `mobile_phone`) VALUES
 (1, 'Cyril', 'EPINAT', 'cyril', '367d233c448abb973a0136207eb0cf446765a6f5', 'epicyr@gmail.com', 1, '1257', '0470707070'),
-(2, 'Arnaud', 'DE LA CRUZ', 'arnaud', 'ea56f45e66e2c57fc79df7dc3ae0437b', 'arnaud.delacruz@gmail.com', 1, '1258', '0470707071'),
-(3, 'Soukeyna', 'GAYE', 'soukeyna', '416c21534a55783c70de4b14b8719851', 'soukeynag@gmail.com', 4, '1259', '0470707072'),
-(4, 'Romain', 'THIVILLON', 'romain', '5026bc63b5418ffdb54f238db245ec01', 'romain.thivillon@gmail.com', 1, '1260', '0470707073'),
-(5, 'Jérémy', 'BARASCUT', 'jeremy', '6967cabefd763ac1a1a88e11159957db', 'jeremy.barascut@gmail.com', 3, '1261', '0470707074'),
-(6, 'Guest', 'Scrumble', 'guest', '084e0343a0486ff05530df6c705c8bb4', 'guestscrumble@gmailcom', 5, NULL, NULL),
-(7, 'Product', 'Owner', 'prodowner', 'f5bf48aa40cad7891eb709fcf1fde128', 'productowner@gmailcom', 4, NULL, NULL),
-(8, 'Scrum', 'Master', 'scrummaster', 'eb0a191797624dd3a48fa681d3061212', 'scrummaster@gmailcom', 3, NULL, NULL),
-(9, 'Developer', 'Scrum', 'scrumdev', '5e8edd851d2fdfbd7415232c67367cc3', 'scrumdeveloper@gmail.com', 2, NULL, NULL),
-(10, 'Admin', 'Scrum', 'adminscrum', '21232f297a57a5a743894a0e4a801fc3', 'adminscrum@gmail.com', 1, NULL, NULL);
+(2, 'Arnaud', 'DE LA CRUZ', 'arnaud', '1a340065606634b575ed162e9c434c27fe24a7c3', 'arnaud.delacruz@gmail.com', 1, '1258', '0470707071'),
+(3, 'Soukeyna', 'GAYE', 'soukeyna', 'b8aabb4b95c817d9df69b6be95b2b94d6b1efe17', 'soukeynag@gmail.com', 4, '1259', '0470707072'),
+(4, 'Romain', 'THIVILLON', 'romain', 'b8aabb4b95c817d9df69b6be95b2b94d6b1efe17', 'romain.thivillon@gmail.com', 1, '1260', '0470707073'),
+(5, 'Jérémy', 'BARASCUT', 'jeremy', 'b3f594e10a9edcf5413cf1190121d45078c62290', 'jeremy.barascut@gmail.com', 3, '1261', '0470707074'),
+(6, 'Guest', 'Scrumble', 'guest', '35675e68f4b5af7b995d9205ad0fc43842f16450', 'guestscrumble@gmailcom', 5, NULL, NULL),
+(7, 'Product', 'Owner', 'prodowner', '7d02c6ad9bad384be2ed6bc2bcc8d8e03e95e7ac', 'productowner@gmailcom', 4, NULL, NULL),
+(8, 'Scrum', 'Master', 'scrummaster', '6978ec3924c878881eb5fffc33a9d8ebbd768310', 'scrummaster@gmailcom', 3, NULL, NULL),
+(9, 'Developer', 'Scrum', 'scrumdev', '0aebcac78827760d5eb302ab20c8d962ce1e92ab', 'scrumdeveloper@gmail.com', 2, NULL, NULL),
+(10, 'Admin', 'Scrum', 'adminscrum', '8b0a5649ef0b35347ccfb45de80316f007beaba3', 'adminscrum@gmail.com', 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -81,6 +82,7 @@ INSERT INTO `memberproject` (`id_project`, `id_member`) VALUES
 (1, 1),
 (1, 2),
 (1, 3),
+(1, 4),
 (1, 5);
 
 -- --------------------------------------------------------
@@ -94,11 +96,6 @@ CREATE TABLE IF NOT EXISTS `planningpoker` (
   `value` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id_planningpoker`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `planningpoker`
---
-
 
 -- --------------------------------------------------------
 
@@ -198,12 +195,12 @@ CREATE TABLE IF NOT EXISTS `sprint` (
 --
 
 INSERT INTO `sprint` (`id_sprint`, `num_sprint`, `title`, `id_project`, `velocity`, `date_start`, `date_end`, `duree`, `id_process_status`) VALUES
-(2, 0, 'Sprint 1', 1, 20, '2012-09-22 01:00:00', '2012-11-21 00:00:00', NULL, 6),
-(3, 0, 'Sprint 2', 1, 12, '2012-11-03 00:00:00', '2012-12-11 00:00:00', NULL, 6),
-(4, 0, 'Sprint 3', 1, 0, '2012-12-11 00:00:00', '2013-01-05 00:00:00', NULL, 5),
-(5, 0, 'Sprint 4', 1, 6, '2013-01-05 00:00:00', '2013-02-10 00:00:00', NULL, 4),
-(6, 0, 'Sprint 5', 1, 10, '2013-02-10 00:00:00', '2013-03-14 00:00:00', NULL, 4),
-(7, 0, 'Sprint 6', 1, 15, '2013-03-14 00:00:00', '2013-05-01 01:00:00', NULL, 4);
+(2, 0, 'Sprint 1', 1, 20, '2012-09-22 01:00:00', '2012-10-06 01:00:00', NULL, 6),
+(3, 0, 'Sprint 2', 1, 12, '2012-10-07 00:00:00', '2012-10-21 00:00:00', NULL, 6),
+(4, 0, 'Sprint 3', 1, 0, '2012-10-22 00:00:00', '2012-11-05 00:00:00', NULL, 5),
+(5, 0, 'Sprint 4', 1, 6, '2012-11-06 00:00:00', '2012-11-20 00:00:00', NULL, 4),
+(6, 0, 'Sprint 5', 1, 10, '2012-11-07 00:00:00', '2012-11-21 00:00:00', NULL, 4),
+(7, 0, 'Sprint 6', 1, 15, '2012-11-22 00:00:00', '2012-12-06 01:00:00', NULL, 4);
 
 -- --------------------------------------------------------
 
@@ -228,33 +225,33 @@ CREATE TABLE IF NOT EXISTS `sprinttaskassignation` (
 --
 
 INSERT INTO `sprinttaskassignation` (`id_task`, `id_sprint`, `id_member`, `date_start`, `date_end`) VALUES
-(33, 2, 1, '2013-06-22 17:13:40', '2013-06-22 17:13:46'),
-(34, 2, 1, '2013-06-22 17:13:41', '2013-06-22 17:13:47'),
-(35, 2, 1, '2013-06-22 17:13:38', '2013-06-22 17:13:44'),
-(36, 2, 1, '2013-06-22 17:13:42', '2013-06-22 17:13:50'),
-(37, 2, 1, '2013-06-22 17:13:38', '2013-06-22 17:13:45'),
-(38, 2, 1, '2013-06-22 17:13:43', '2013-06-22 17:13:49'),
-(39, 2, 1, '2013-06-22 17:13:51', '2013-06-22 17:13:51'),
-(40, 2, 1, '2013-06-22 17:13:53', '2013-06-22 17:13:53'),
-(41, 2, 1, '2013-06-22 17:13:52', '2013-06-22 17:13:52'),
-(42, 2, 1, '2013-06-22 17:13:55', '2013-06-22 17:13:55'),
-(43, 2, 1, '2013-06-22 17:13:54', '2013-06-22 17:13:54'),
-(44, 2, 1, '2013-06-22 17:13:56', '2013-06-22 17:13:56'),
-(50, 3, 1, '2013-06-22 17:15:10', '2013-06-22 17:15:10'),
-(51, 3, 1, '2013-06-22 17:15:14', '2013-06-22 17:15:14'),
-(59, 3, 1, '2013-06-22 17:15:16', '2013-06-22 17:15:16'),
-(60, 3, 1, '2013-06-22 17:15:17', '2013-06-22 17:15:17'),
-(61, 3, 1, '2013-06-22 17:15:18', '2013-06-22 17:15:18'),
-(62, 3, 1, '2013-06-22 17:15:19', '2013-06-22 17:15:19'),
-(63, 3, 1, '2013-06-22 17:15:20', '2013-06-22 17:15:20'),
-(64, 3, 1, '2013-06-22 17:15:21', '2013-06-22 17:15:21'),
-(69, 4, 1, '2013-06-22 17:15:46', NULL),
-(70, 4, 1, '2013-06-22 17:15:48', '2013-06-22 17:15:48'),
-(71, 4, 1, '2013-06-22 17:15:47', NULL),
-(78, 4, 1, '2013-06-22 17:15:50', NULL),
-(79, 4, 1, '2013-06-22 17:15:51', NULL),
-(80, 4, 1, '2013-06-22 17:15:53', '2013-06-22 17:15:53'),
-(81, 4, 1, '2013-06-22 17:15:55', '2013-06-22 17:15:55');
+(33, 2, 1, '2012-09-22 17:13:40', '2012-09-23 17:13:46'),
+(34, 2, 1, '2012-09-22 17:13:41', '2012-09-24 17:13:47'),
+(35, 2, 1, '2012-09-23 17:13:38', '2012-09-25 17:13:44'),
+(36, 2, 1, '2012-09-26 17:13:42', '2012-09-28 17:13:50'),
+(37, 2, 1, '2012-09-25 17:13:38', '2012-09-28 17:13:45'),
+(38, 2, 1, '2012-09-29 17:13:43', '2012-10-04 17:13:49'),
+(39, 2, 1, '2012-10-01 17:13:51', '2012-10-04 17:13:51'),
+(40, 2, 1, '2012-10-02 12:13:53', '2012-10-02 17:13:53'),
+(41, 2, 1, '2012-10-02 17:13:52', '2012-10-06 17:13:52'),
+(42, 2, 1, '2012-10-01 09:13:55', '2012-10-01 12:13:55'),
+(43, 2, 1, '2012-10-02 14:13:54', '2012-10-02 17:13:54'),
+(44, 2, 1, '2012-10-03 17:13:56', '2013-10-06 17:13:56'),
+(50, 3, 1, '2012-10-07 08:15:10', '2012-10-07 17:15:10'),
+(51, 3, 1, '2012-10-07 08:15:14', '2012-10-10 17:15:14'),
+(59, 3, 1, '2012-10-07 08:15:16', '2012-10-07 17:15:16'),
+(60, 3, 1, '2012-10-08 17:15:17', '2012-10-12 17:15:17'),
+(61, 3, 1, '2012-10-07 17:15:18', '2012-10-12 17:15:18'),
+(62, 3, 1, '2012-10-12 08:15:19', '2012-10-14 17:15:19'),
+(63, 3, 1, '2012-10-14 17:15:20', '2012-10-16 17:15:20'),
+(64, 3, 1, '2012-10-16 17:15:21', '2012-10-21 17:15:21'),
+(70, 4, 1, '2012-10-22 17:15:48', NULL),
+(71, 4, 1, '2012-10-22 08:15:47', '2012-10-24 11:32:49'),
+(76, 4, 1, '2012-10-23 11:32:57', NULL),
+(77, 4, 1, '2012-10-22 11:32:55', '2012-10-24 12:00:26'),
+(78, 4, 1, '2012-10-24 17:15:50', NULL),
+(80, 4, 1, '2012-10-23 12:00:27', NULL),
+(81, 4, 1, '2012-10-22 08:15:55', '2012-10-24 17:15:55');
 
 -- --------------------------------------------------------
 
@@ -271,7 +268,7 @@ CREATE TABLE IF NOT EXISTS `task` (
   PRIMARY KEY (`id_task`),
   KEY `fk_id_userstory_idx` (`id_userstory`),
   KEY `fk_task_1_idx` (`id_process_status`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=83 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=82 ;
 
 --
 -- Dumping data for table `task`
@@ -320,18 +317,18 @@ INSERT INTO `task` (`id_task`, `title`, `estimation`, `id_userstory`, `id_proces
 (66, 'Form Member', 1, 16, 4),
 (67, 'Web Services Connection', 2, 16, 4),
 (68, 'List all project members', 1, 17, 4),
-(69, 'List all member which not project member', 1, 17, 5),
-(70, 'Add project member', 1, 17, 6),
-(71, 'Delete project member', 1, 17, 5),
+(69, 'List all member which not project member', 1, 17, 4),
+(70, 'Add project member', 1, 17, 5),
+(71, 'Delete project member', 1, 17, 6),
 (72, 'web connection ', 2, 18, 4),
 (73, 'List member in project', 1, 18, 4),
 (74, 'Delete member in project', 1, 18, 4),
 (75, 'Autocomplete in add form', 2, 18, 4),
-(76, 'List All task', 1, 19, 4),
-(77, 'Search task', 2, 19, 4),
+(76, 'List All task', 1, 19, 5),
+(77, 'Search task', 2, 19, 6),
 (78, 'Add task', 1, 19, 5),
-(79, 'Delete task', 1, 19, 5),
-(80, 'Show task', 1, 19, 6),
+(79, 'Delete task', 1, 19, 4),
+(80, 'Show task', 1, 19, 5),
 (81, 'Modify task', 1, 19, 6);
 
 -- --------------------------------------------------------
@@ -459,3 +456,7 @@ ALTER TABLE `userstory`
 ALTER TABLE `userstorysprint`
   ADD CONSTRAINT `fk_ustorysprint_sprint` FOREIGN KEY (`id_sprint`) REFERENCES `sprint` (`id_sprint`) ON DELETE CASCADE ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_ustorysprint_userstory` FOREIGN KEY (`id_userstory`) REFERENCES `userstory` (`id_userstory`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
